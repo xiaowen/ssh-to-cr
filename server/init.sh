a17e3476fa9d6ae5f311b8510323ce1a6de9518c
x <x>
2021-08-02 00:02:56 -0700

    server/init.sh: run the web server as appuser, so appuser can later kill it too

--- server/init.sh
+++ server/init.sh
@@ -3,8 +3,8 @@
 echo "Starting SSH server ..."
 service ssh start
 
-echo "Starting Envoy ..."
-/app/envoy -c /etc/envoy/server-envoy.yaml &
-
 echo "Starting web server"
-python3 -m http.server --bind 0.0.0.0 8080
+su - appuser -c 'python3 -m http.server --bind 0.0.0.0 8080' &
+
+echo "Starting Envoy ..."
+/app/envoy -c /etc/envoy/server-envoy.yaml
